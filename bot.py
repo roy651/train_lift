c24e144e65554d01cf869ebf94d11d7c6e2b416d
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2022-12-15 21:53:09 +0200

    back to english

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ version_id = "0.0.1"
 # set up the introductory statement for the bot when the /start command is invoked
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="/מסיע או /נוסע")
+    context.bot.send_message(chat_id=chat_id, text="/canpickup OR /needride")
 
 def version(update, context):
     print("Version is:" + version_id)
@@ -25,9 +25,9 @@ def pickup_command(update, context):
     # Get the chat id
     chat_id = update.message.chat_id
     # Create the keyboard with the 2 options
-    keyboard = [['מהר גילון-לרכבת אחיהוד', 'מרכבת אחיהוד-להר גילון']]
+    keyboard = [['to-train', 'from-train']]
     # Send a message to the user with the keyboard
-    context.bot.send_message(chat_id=chat_id, text='בחרו רכבת:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
+    context.bot.send_message(chat_id=chat_id, text='Select the direction:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
 
 # Define the function to handle the to-train and from-train options
 def pick_train(update, context):
@@ -36,9 +36,9 @@ def pick_train(update, context):
     # Get the selected option
     option = update.message.text
     # Create the keyboard with the 2 options
-    keyboard = [['היום', 'מחר']]
+    keyboard = [['today', 'tomorrow']]
     # Send a message to the user with the keyboard
-    context.bot.send_message(chat_id=chat_id, text='בחרו יום:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
+    context.bot.send_message(chat_id=chat_id, text='Select day:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
 
 # Define the function to handle the today and tomorrow options
 def pick_date(update, context):
@@ -49,7 +49,7 @@ def pick_date(update, context):
     # Create the keyboard with the time options
     keyboard = [['6:06 AM', '6:39 AM'], ['07:06 AM', '07:39 AM'], ['08:06 AM', '08:39 AM']]
     # Send a message to the user with the keyboard
-    context.bot.send_message(chat_id=chat_id, text='בחרו שעת רכבת:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
+    context.bot.send_message(chat_id=chat_id, text='Select train time:', reply_markup=telegram.ReplyKeyboardMarkup(keyboard))
 
 
 
@@ -65,15 +65,13 @@ if telegram_bot_token != 'TEST':
     dispatcher.add_handler(CommandHandler("start", start))
 
     # Add the command handler for the /pickup command
-    dispatcher.add_handler(CommandHandler('מסיע', pickup_command))
-
-    dispatcher.add_handler(CommandHandler('נוסע', pickup_command))
+    dispatcher.add_handler(CommandHandler('canpickup', pickup_command))
 
     # Add the message handler for the to-train and from-train options
-    dispatcher.add_handler(MessageHandler(Filters.regex('^(מהר גילון-לרכבת אחיהוד|מרכבת אחיהוד-להר גילון)$'), pick_train))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(to-train|from-train)$'), pick_train))
 
     # Add the message handler for the today and tomorrow options
-    dispatcher.add_handler(MessageHandler(Filters.regex('^(היום|מחר)$'), pick_date))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(today|tomorrow)$'), pick_date))
 
     # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
